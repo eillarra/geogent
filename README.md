261c71a35bfc973e006dc6760762a1059ed7b311
Eneko Illarramendi <eneko@illarra.com>
2016-10-05 19:51:37 +0200

    Add some basic routes

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-Improved GeoJSON datasets for Ghent  
-===================================
+Improved GeoJSON datasets for data.stad.gent  
+============================================
 
 [![license-badge]](LICENSE)
 
 The [Open Data portal of the City of Ghent](https://data.stad.gent/datasets) has a collection of
 datasets can can be downloaded in different formats, like XML, KML, JSON or GeoJSON. The **problem with
 the GeoJSON responses** is that they normally only contain `MultiPolygon` or `MultiPoint` collections
-of coordinates and very little extra information.
+of coordinates and very little extra information (it seems the City of Ghent is not using the latest version
+of [The DataTank](http://thedatatank.com/), that fixes this).
 
 This simple application reads the associated KML files to **generate more complete GeoJSON** responses.
 
 How to use it
 -------------
 You can access the data at [https://geogent.herokuapp.com/](https://geogent.herokuapp.com/).
-To make a request you just need to replace the City of Ghent Datatank domain `datatank.stad.gent` with
+To make a request you just need to replace the City of Ghent DataTank domain `datatank.stad.gent` with
 `geogent.herokuapp.com`. For example, the following URLs:
 
     https://datatank.stad.gent/4/cultuursportvrijetijd/speelterreinen.geojson
--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
+import json
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, Response, send_from_directory
 from flask_cache import Cache
 from geojson import FeatureCollection, Feature, Point, Polygon
 from lxml import etree
@@ -17,6 +18,8 @@ else:
     DEBUG = True
     CACHE_CONFIG = {'CACHE_TYPE': 'null'}
 
+DATATANK = 'https://datatank.stad.gent/4/'
+
 
 app = Flask(__name__)
 cache = Cache(app, config=CACHE_CONFIG)
@@ -30,44 +33,70 @@ def parse_coordinates(string):
     return Point(points[0]) if len(points) == 1 else Polygon([points])
 
 
-@app.route('/<int:v>/<string:category>/<string:dataset>.geojson')
-@cache.cached(timeout=5 * 60 * 60)
-def get_dataset(v, category, dataset):
-    datatank = 'https://datatank.stad.gent/'
-    key = str(v) + '/' + category + '/' + dataset
+@app.errorhandler(404)
+def page_not_found(e):
+    res = json.dumps({
+        'error': 'Sorry, but we could not find the requested page'
+    })
+
+    return Response(response=res, status=404, mimetype='application/json')
+
+
+@app.route('/4/<string:category>/<string:dataset>.geojson')
+@cache.cached(timeout=24 * 60 * 60)
+def get_dataset(category, dataset):
+    key = category + '/' + dataset
 
     try:
-        source = get(datatank + key + '.kml').text.encode('utf-8')
+        source = get(DATATANK + key + '.kml').text.encode('utf-8')
         tree = etree.fromstring(source, parser=etree.XMLParser(ns_clean=True))
-    except:
-        return jsonify({
-            'error': 'Sorry, but we could not find the requested dataset'
-        }), 404
+    except Exception as e:
+        return page_not_found(e)
 
     nsm = tree.nsmap
     nsm['kml'] = nsm.pop(None)
     features = []
 
-    for element in tree.xpath('//kml:Placemark', namespaces=nsm):
+    for el in tree.xpath('//kml:Placemark', namespaces=nsm):
         properties = {}
 
-        for d in element.xpath('.//kml:SimpleData', namespaces=nsm):
-            value = d.text
+        for prop in el.xpath('.//kml:SimpleData', namespaces=nsm):
+            value = prop.text
             try:
-                properties[d.attrib['name'].lower()] = int(value) if value.isdigit() else value
+                properties[prop.attrib['name'].lower()] = int(value) if value.isdigit() else value
             except:
-                properties[d.attrib['name'].lower()] = None
+                properties[prop.attrib['name'].lower()] = None
 
         if 'naam' in properties:
             properties['title'] = properties['naam']
 
-        geometry = parse_coordinates(element.find('.//kml:coordinates', namespaces=nsm).text)
+        geometry = parse_coordinates(el.find('.//kml:coordinates', namespaces=nsm).text)
         features.append(Feature(geometry=geometry, properties=properties))
 
-    return jsonify(FeatureCollection(features, properties={
+    res = json.dumps(FeatureCollection(features, properties={
         'title': tree.xpath('//kml:Document/kml:Folder/kml:name', namespaces=nsm)[0].text,
-        'source': datatank + key + '.geojson',
+        'source': DATATANK + key,
     }))
+
+    return Response(response=res, status=200, mimetype='application/vnd.geo+json')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
+
+
+@app.route('/4/')
+@app.route('/')
+@cache.cached(timeout=30 * 24 * 60 * 60)
+def index():
+    res = json.dumps({
+        'info': 'Check the Open Data portal of the City of Ghent for available datasets; then, simply replace '
+                '`datatank.stad.gent` for `geogent.herokuapp.com` when requesting GeoJSON datasets.',
+        'url': DATATANK
+    })
+
+    return Response(response=res, status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
